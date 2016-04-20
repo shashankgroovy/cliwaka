a6c52442baabe599ecd6eda85a07d43c2e04d881
Shashank Srivastav <shashankgrovy@gmail.com>
2016-04-20 19:23:04 +0530

    we now retreive data from every api point

--- wakacli/cli.py
+++ wakacli/cli.py
@@ -13,36 +13,85 @@ resource_endpoints = {
     'stats': '/stats/last_7_days',
     'summaries' : '/summaries',
     'durations': '/durations',
-    'hearbeats': '/heartbeats'
+    'heartbeats': '/heartbeats',
+    'user_agents': '/user_agents'
     }
 
-def get_user(user="current"):
+def view_user(user="current"):
     """Returns a specific users information and returns the current user by
     default."""
-    pass
+    if user != 'current':
+        user = '@' + user
+    make_request(user=user)
+
+def view_leaderboard():
+    """Returns the public leaderboard.
+
+    A list of users ranked by logged time in descending order."""
+    make_request(leader=True)
 
 def get_stats():
-    """Returns current users information"""
+    """Returns user's stats and logged time for the given time range.
+
+    Allowed time ranges are:
+        last_7_days,            # only a weeks stat is available for free accounts
+        last_30_days,
+        last_6_months,
+        last_year,
+        all_time
+    """
     pass
 
-def get_summaries():
-    """Returns current users information"""
-    pass
+def get_summaries(start=None, end=None, project="", branches=""):
+    """Returns a user's logged time for the given time range as an array of
+    summaries segmented by day."""
+    if start == None:
+        import datetime
+        start = datetime.date.today()
+
+    if end == None:
+        import datetime
+        end = datetime.date.today()
+
+    endpoint = 'summaries'
+    params = {
+        'start': start,
+        'end': end,
+        'project': project,
+        'branches': branches
+    }
+    make_request(endpoint, params)
 
 def get_durations(date=None, project="", branches=""):
     """Returns a user's logged time for the given day as an array of duration
     blocks."""
     if date == None:
         import datetime
-        date = str(datetime.date.today())
+        date = datetime.date.today()
 
     endpoint = 'durations'
+    params = {
+        'date': date,
+        'project': project,
+        'branches': branches
+    }
+    make_request(endpoint, params)
+
+def get_heartbeats(date=None):
+    """Returns a user's heartbeats sent from plugins for the given day as an
+    array."""
+    if date == None:
+        import datetime
+        date = str(datetime.date.today())
+
+    endpoint = 'heartbeats'
     params = {'date': date}
     make_request(endpoint, params)
 
-def request_heartbeats():
-    """Returns current users information"""
-    pass
+def get_user_agents():
+    """Returns a list of plugins which have sent data for this user."""
+    endpoint = 'user_agents'
+    make_request(endpoint)
 
 def make_request(endpoint="", params={}, user="current", leader=False):
     """Generic request wrapper for routing all requests to api"""
@@ -60,12 +109,17 @@ def make_request(endpoint="", params={}, user="current", leader=False):
             endpoint = resource_endpoints[endpoint]
 
         r = requests.get(BASE_URL + user_type + endpoint, params=params)
-        print pretty_output(json.dumps(r.json()))
+
+        if r.status_code == 200:
+            print pretty_output(json.dumps(r.json()))
+        elif r.status_code == 403:
+            print "Whoops! Looks like we can't show that."
+        else:
+            print "We are having some problem. Make sure you have everything correct."
 
     else:
         r = requests.get(BASE_URL + 'leaders')
         print pretty_output(json.dumps(r.json()))
-
 
 # handle SIGINT
 def signal_handler(signal, frame):
@@ -82,4 +136,4 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     # run
-    make_request()
+    get_user()
