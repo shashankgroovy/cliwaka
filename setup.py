d8a5fad949e1fafe25f106f8158e6ea979cd2524
Shashank Srivastav <shashankgrovy@gmail.com>
2016-04-24 22:14:21 +0530

    Add setup files for pypi

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+current_dir = path.abspath(path.dirname(__file__))
+with open(path.join(current_dir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cliwaka',
+    version='0.1.1',
+    description='Cliwaka is an interactive command line utility for wakatime.com',
+    long_description=long_description,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+
+    author='Shashank Srivastav',
+    author_email='shashankgrovy@gmail.com',
+
+    license='GPLv3',
+    keywords='wakatime cli tool',
+    url='https://github.com/shashankgroovy/cliwaka',
+    download_url='https://github.com/shashankgroovy/cliwaka/archive/master.zip',
+    website='https://github.com/shashankgroovy/cliwaka',
+
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'argparse'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'cliwaka=cliwaka:main',
+        ],
+    },
+)
